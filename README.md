4fce23d962089718f739861945be3617fc486af1
mickdiggin <m_degan@mail.fhsu.edu>
2024-04-01 14:59:30 -0500

    Final unstable build. The HTML image tags associated with each database entry will display correctly in HTML viewer web apps, but will not display in any web browser. Also added a README and requirements.txt file.

--- README.md
+++ README.md
@@ -1,11 +1,60 @@
-# miniproject3MichaelDeGan
+### INF601 - Advanced Programming in Python
+### Michael DeGan
+### Mini Project 3
 
 
+# Mini Project 3
+## Description
 
+This project utilizes Python and Flask to build and manage a database of movies that can be accessed freely by anyone,
+and modified by any registered user.
 
-Setup database
+## Getting Started
+
+### Install requirements with PIP
+
+```
+pip install -r requirements.txt
+```
+### Setup your local environment
+Download the project source files into a directory of your choice, select it as the current working directory and 
+enter the following command:
+```
 flask --app flaskr init-db
+```
+### Executing the app
 
-
-Run application
+As this application has not yet been deployed into a production environment, you must run it locally on a
+development server:
+```
 flask --app flaskr run
+```
+The app can then be accessed from a web browser of your choice at the following address: http://127.0.0.1:5000
+
+## Help
+If you experience any errors getting your dev server up and running, Flask has 
+[excellent troubleshooting documentation](https://flask.palletsprojects.com/en/2.3.x/server/).
+
+## Authors
+
+Contributors names and contact info
+
+Michael DeGan
+
+## Version History
+
+* 0.1
+    * Initial Release
+
+## License
+
+This project is public domain because I am a saintly man. Proprietary components, such as the various modules, are under 
+various licenses.
+
+## Acknowledgments
+
+Inspiration, code snippets, etc.
+* [stackoverflow](https://stackoverflow.com/)
+* [python.org](https://docs.python.org/3/)
+* [ChatGPT](https://chat.openai.com/)
+* [Code Beautify](https://codebeautify.org/htmlviewer)
--- flaskr/blog.py
+++ flaskr/blog.py
@@ -6,15 +6,52 @@ from werkzeug.exceptions import abort
 from flaskr.auth import login_required
 from flaskr.db import get_db
 
+import base64
+import imghdr  # Import imghdr module for determining image format
+
 bp = Blueprint('blog', __name__)
 
+# Template function to generate HTML for displaying movie poster
+@bp.app_template_global()
+def check_movie_poster(image_data):
+    if image_data:
+        # Determine the image format dynamically
+        image_format = imghdr.what(None, h=image_data)
+    if image_format:
+        return image_format
+    else:
+        return None
 
+
+@bp.app_template_global()
+def encode_movie_poster(image_data):
+    if image_data:
+        verified = check_movie_poster(image_data)
+        # Convert binary image data to Base64 encoding
+        if verified:
+            base64_image = base64.b64encode(image_data)
+
+            # Return HTML with embedded Base64-encoded image data and determined image format
+            return base64_image
+        else:
+            return None
+    else:
+        return None
+
+
+@bp.app_template_global()
+def build_poster_string(image_data):
+    verified = check_movie_poster(image_data)
+    if verified:
+        encoding = encode_movie_poster(image_data)
+        if encoding:
+            return "<img src=\"data:image/" + verified + ";base64, " + encoding.decode('utf-8') + "\" alt=\"Movie Poster\">"
 @bp.route('/')
 def index():
     db = get_db()
     entries = db.execute(
-        'SELECT e.id, title, release_year, starring, synopsis, edited, author_id, username'
-        ' FROM entry e JOIN user u ON e.author_id = u.id'
+        'SELECT e.id, title, release_year, starring, synopsis, data, edited, author_id, username'
+        ' FROM entry AS e LEFT JOIN images AS i ON e.id = i.movie_id INNER JOIN user AS u ON e.id = u.id'
         ' ORDER BY edited DESC'
     ).fetchall()
     return render_template('blog/index.html', entries=entries)
@@ -28,6 +65,7 @@ def create():
         releaseYr = request.form['release_year']
         starring = request.form['starring']
         synopsis = request.form['synopsis']
+        poster = request.files['poster']
         error = None
 
         if not title:
@@ -44,6 +82,14 @@ def create():
                 ' VALUES (?, ?, ?, ?, ?)',
                 (title, releaseYr, starring, synopsis, g.user['id'])
             )
+            if poster is not None:
+                theEntry = db.execute(
+                            'SELECT e.id, author_id, title'
+                            ' FROM entry e'
+                            ' WHERE e.title = ?', (title,)).fetchone()
+                db.execute('INSERT INTO images (filename, data, user_id, movie_id)'
+                           ' VALUES (?, ?, ?, ?)',
+                           (poster.filename, poster.read(), g.user['id'], theEntry['id'],))
             db.commit()
             return redirect(url_for('blog.index'))
 
@@ -74,6 +120,7 @@ def update(id):
         release_year = request.form['release_year']
         starring = request.form['starring']
         synopsis = request.form['synopsis']
+        poster = request.files['poster']
         error = None
 
         if not title:
@@ -90,6 +137,15 @@ def update(id):
                 ' WHERE id = ?',
                 (title, release_year, starring, synopsis, g.user['id'], id)
             )
+            if poster is not None:
+                theEntry = db.execute(
+                    'SELECT e.id, author_id, title'
+                    ' FROM entry e'
+                    ' WHERE e.title = ?', (title,)).fetchone()
+                db.execute('UPDATE images SET filename = ?, data = ?, user_id = ?'
+                           ' WHERE id = ?',
+                           (poster.filename, poster.read(), g.user['id'], theEntry['id'],))
+
             db.commit()
             return redirect(url_for('blog.index'))
 
